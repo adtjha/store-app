5bdd3c095538d566d3c6330ab9403265da57e972
adtjha <adtjha@hotmail.com>
2022-05-07 13:30:34 +0530

    base setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
+import { StoreModule } from '@ngrx/store';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ManageProductsComponent } from './manage-products/manage-products.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ProductListComponent, HomeComponent, ProductComponent, ManageProductsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'manage', component: ManageProductsComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
